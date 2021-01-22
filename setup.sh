2f66817be4bcd1a49680c0ae71c3d016b272ceb7
Äkwav <ekwav@coflnet.com>
2021-01-22 18:28:55 +0100

    switched to containerd

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 # devtools
 pacman -S --noconfirm nano htop git
 # dependencies
-pacman -S --noconfirm curl cri-o # etcd should be installed as per wiki
+pacman -S --noconfirm curl #cri-o # etcd should be installed as per wiki
 
 #master
 pacman -S --noconfirm  kubernetes-control-plane 
@@ -10,7 +10,7 @@ pacman -S --noconfirm  kubernetes-control-plane
 #pacman -S --noconfirm  kubernetes-node
 
 # setup stuff
-pacman -S --noconfirm kubectl kubeadm
+pacman -S --noconfirm containerd kubectl kubeadm
 
 echo "enabling kubelet"
 systemctl enable kubelet.service crio.service
@@ -21,6 +21,20 @@ swapoff -a
 cp coflnet-start.service /lib/systemd/system/coflnet-start.service
 sudo systemctl enable coflnet-start
 
+
+cat <<EOF | tee /etc/modules-load.d/containerd.conf
+br_netfilter
+EOF
+ modprobe br_netfilter
+cat <<EOF | tee /etc/sysctl.d/99-kubernetes-cri.conf
+net.bridge.bridge-nf-call-iptables  = 1
+net.ipv4.ip_forward                 = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+EOF
+
+sysctl --system
+systemctl start containerd
+systemctl enable containerd
 
 # change group
 echo " --cgroup-driver='systemd'" >> /etc/kubernetes/kubelet.env
